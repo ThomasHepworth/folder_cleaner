ef107bfe801e6729a4099a621d03e79858c73c44
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2024-03-23 15:25:50 +0000

    Squash `load_cli_configs` into `extract_user_config_from_path`

--- src/configs/parsing.rs
+++ src/configs/parsing.rs
@@ -6,18 +6,43 @@ use std::path::PathBuf;
 use toml;
 use toml::de::Error;
 
-type ConfigUnpacked = (SizeConfig, Option<PathConfigMap>);
+type ConfigsUnpacked = (SizeConfig, Option<PathConfigMap>);
 
 fn read_config_file(config_file_path: &PathBuf) -> Result<String, ConfigError> {
     fs::read_to_string(config_file_path).map_err(|_| ConfigError::read_error(config_file_path))
 }
 
-fn parse_config_from_str(file_content: &str) -> Result<ConfigUnpacked, Error> {
+fn parse_config_from_str(file_content: &str) -> Result<ConfigsUnpacked, Error> {
     let config: Config = toml::from_str(file_content)?;
     Ok((config.size.unwrap_or_default(), config.subgroups))
 }
 
-pub fn load_and_parse_config(config_file_path: PathBuf) -> Result<ConfigUnpacked, ConfigError> {
+/// Extracts the user configuration from a specified configuration file.
+///
+/// This function attempts to locate and parse a user-specific configuration file,
+/// returning the configuration settings it contains. It first constructs the file path
+/// using `get_user_config_path` with the provided `config_filename`, then parses
+/// the configuration file into structured data.
+///
+/// # Arguments
+///
+/// * `config_file_path` - A PathBuf that holds the full path to the configuration file.
+///
+/// # Returns
+///
+/// This function returns a `Result` which is either:
+/// - `Ok((SizeConfig, Option<PathConfigMap>))` containing the parsed configuration data if successful.
+/// - `Err(ConfigError)` describing the type of error encountered (e.g., file not found, parse error).
+///
+/// # Examples
+///
+/// ```
+/// let user_config = extract_user_config_from_path("app_config.toml")
+///     .expect("Failed to extract user config");
+/// ```
+pub fn extract_user_config_from_path(
+    config_file_path: &PathBuf,
+) -> Result<ConfigsUnpacked, ConfigError> {
     let file_content = read_config_file(&config_file_path)
         .map_err(|_| ConfigError::read_error(&config_file_path))?;
 
@@ -33,7 +58,7 @@ mod tests {
 
     // Generic function that creates a temporary TOML file, writes contents to it,
     // and then applies a provided function to the file. It returns the result of that function.
-    fn create_temp_toml_file_and_read_or_parse<F, T>(contents: &str, action: F) -> (T, String)
+    fn create_temp_toml_file_and_map<F, T>(contents: &str, action: F) -> (T, String)
     where
         F: FnOnce(PathBuf) -> T,
     {
@@ -56,10 +81,14 @@ mod tests {
         let toml_str = r#"
             [[downloads]]
             directory = "/example/downloads"
-            extensions_to_del = ["tmp", "log"]
+            extensions_to_del = ["tmp", "log", "rs"]
 
             [[documents]]
             directory = "/example/documents"
+            extensions_to_keep = ["pdf", "docx"]
+            [[documents]]
+            directory = "/example/docs"
+            extensions_to_keep = ["xlsx", "pptx"]
         "#;
 
         let (_, subgroups) = parse_config_from_str(toml_str).unwrap();
@@ -68,11 +97,32 @@ mod tests {
             subgroups.get("downloads").unwrap()[0].directory,
             PathBuf::from("/example/downloads")
         );
-        // TODO: Fix test
-        // assert_eq!(
-        //     config.subgroups.get("documents").unwrap()[0].extensions_to_del,
-        //     vec!["tmp".to_string(), "log".to_string()]
-        // );
+        assert_eq!(
+            subgroups.get("downloads").unwrap()[0]
+                .extensions_to_del
+                .as_ref()
+                .unwrap(),
+            &vec!["tmp".to_string(), "log".to_string(), "rs".to_string()]
+        );
+        assert!(subgroups.get("downloads").unwrap()[0]
+            .extensions_to_keep
+            .is_none());
+
+        // For "documents" subgroup, check extensions_to_keep instead of extensions_to_del
+        assert_eq!(
+            subgroups.get("documents").unwrap()[0]
+                .extensions_to_keep
+                .as_ref()
+                .unwrap(),
+            &vec!["pdf".to_string(), "docx".to_string()]
+        );
+        assert_eq!(
+            subgroups.get("documents").unwrap()[1]
+                .extensions_to_keep
+                .as_ref()
+                .unwrap(),
+            &vec!["xlsx".to_string(), "pptx".to_string()]
+        );
     }
 
     #[test]
@@ -112,28 +162,14 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_fail_due_to_missing_directory() {
-        let toml_str = r#"
-            [[downloads]]
-            directory = "/example/downloads"
-            [[documents]]
-        "#;
-
-        match parse_config_from_str(toml_str) {
-            Err(_) => (), // Test passes if an error is returned
-            _ => panic!("Expected a toml::de::Error"),
-        }
-    }
-
-    #[test]
     fn test_load_and_parse_invalid_toml() {
         let invalid_toml = r#"
             [[path]
             directory = "/example/path"
         "#; // This TOML string is intentionally invalid
 
-        let (result, _) = create_temp_toml_file_and_read_or_parse(invalid_toml, |file_path| {
-            load_and_parse_config(file_path)
+        let (result, _) = create_temp_toml_file_and_map(invalid_toml, |file_path| {
+            extract_user_config_from_path(&file_path)
         });
 
         match result {
@@ -141,4 +177,11 @@ mod tests {
             _ => panic!("Test failed, unexpected result: {:#?}", result),
         }
     }
+
+    // TODO: Extend tests to cover more cases!
+    // - Test all of our potential sources of error while deserialising:
+    // - Test that the function returns the correct error when the file is not found
+    // - Test that the function returns the correct error when the file is not readable
+    // - Test that the function returns correctly parsed values in more cases
+    // - Validate deserialise errors are handled correctly - e.g. missing fields, wrong types
 }
