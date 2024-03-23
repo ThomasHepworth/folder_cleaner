1918d693891f653147eefa79d7b83ca892dad23c
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2024-03-23 16:28:35 +0000

    Move `extract_user_config` to the parsing file

--- src/configs.rs
+++ src/configs.rs
@@ -2,10 +2,9 @@ pub mod config;
 pub mod config_error;
 pub mod parsing;
 
-use config::{PathConfigMap, SizeConfig};
 use config_error::ConfigError;
 use directories::UserDirs;
-use parsing::load_and_parse_config;
+use parsing::extract_user_config_from_path;
 use std::path::PathBuf;
 
 pub const CONFIG_FILE_NAME: &str = ".nuke.toml";
@@ -32,7 +31,7 @@ pub const CONFIG_FILE_NAME: &str = ".nuke.toml";
 /// let config_path = get_user_config_path("app_config.toml").expect("Failed to get config path");
 /// println!("Config path: {:?}", config_path);
 /// ```
-fn get_user_config_path(config_filename: &str) -> Result<PathBuf, ConfigError> {
+pub fn get_user_config_path(config_filename: &str) -> Result<PathBuf, ConfigError> {
     let user_dirs = UserDirs::new().ok_or(ConfigError::UserDirNotFound)?;
     let user_dir = user_dirs.home_dir();
     let config_file_path = user_dir.join(config_filename);
@@ -45,35 +44,4 @@ pub fn report_user_config_path(config_filename: &str) {
         Ok(path) => println!("User config path: {:?}", path),
         Err(e) => eprintln!("Error: {}", e),
     }
-}
-
-/// Extracts the user configuration from a specified configuration file.
-///
-/// This function attempts to locate and parse a user-specific configuration file,
-/// returning the configuration settings it contains. It first constructs the file path
-/// using `get_user_config_path` with the provided `config_filename`, then parses
-/// the configuration file into structured data.
-///
-/// # Arguments
-///
-/// * `config_filename` - A string slice that holds the name of the configuration file.
-///
-/// # Returns
-///
-/// This function returns a `Result` which is either:
-/// - `Ok((SizeConfig, Option<PathConfigMap>))` containing the parsed configuration data if successful.
-/// - `Err(ConfigError)` describing the type of error encountered (e.g., file not found, parse error).
-///
-/// # Examples
-///
-/// ```
-/// let user_config = extract_user_config("app_config.toml")
-///     .expect("Failed to extract user config");
-/// println!("User config extracted successfully.");
-/// ```
-pub fn extract_user_config(
-    config_filename: &str,
-) -> Result<(SizeConfig, Option<PathConfigMap>), ConfigError> {
-    let user_config_file_path = get_user_config_path(config_filename)?;
-    load_and_parse_config(user_config_file_path)
 }
